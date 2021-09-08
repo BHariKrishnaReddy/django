88b3b3c203839e8d30aa05ae24f950c68a37b9ea
BHariKrishnaReddy <reddyfrn007@gmail.com>
2021-09-08 20:21:55 +0530

    add jobs and bootstrap functionality

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+#creat a blog model
+class Blog(models.Model):
+	title = models.CharField(max_length = 100)
+	pub_date = models.DateTimeField()
+	body = models.CharField(max_length = 200)
+	image = models.ImageField(upload_to = 'images/')
+
+
+
+#add blog model to settings,creat migration,migrate,add admin
+
